b3eea80eb2d3ae4de05436b876a6984a80e924e4
Tim Van der Perre <tim.vanderperre@student.ucll.be>
2021-05-07 10:29:17 +0200

    temperature-scale-vm-werkt

--- TemperatureConverter/View/ViewModel.cs
+++ TemperatureConverter/View/ViewModel.cs
@@ -1,28 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
-
 
 namespace View
 {
-    class ConverterViewModel : INotifyPropertyChanged
+    public class ConverterViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
         private double temperatureInKelvin;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ConverterViewModel()
+        {
+            this.Kelvin = new TemperatureScaleViewModel(this, new KelvinTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
+        }
+
         public double TemperatureInKelvin
         {
-            get
-            {
-                return temperatureInKelvin;
-            }
+            get{return temperatureInKelvin;}
             set
             {
                 temperatureInKelvin = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(temperatureInKelvin)));
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
             }
         }
+
+        public TemperatureScaleViewModel Kelvin { get; }
+
+        public TemperatureScaleViewModel Celsius { get; }
+
+        public TemperatureScaleViewModel Fahrenheit { get; }
+    }
+
+    public class TemperatureScaleViewModel : INotifyPropertyChanged
+    {
+        private readonly ConverterViewModel parent;
+
+        private readonly ITemperatureScale temperatureScale;
+
+        public TemperatureScaleViewModel(ConverterViewModel parent, ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+
+            this.parent.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+        }
+
+        public string Name => temperatureScale.Name;
+
+        public double Temperature
+        {
+            get{return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin);}
+            set{parent.TemperatureInKelvin = temperatureScale.ConvertToKelvin(value);}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
